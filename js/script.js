bab32c7486a0441d8047546b42d8f22144754747
BE <bernicewebdesigner@gmail.com>
2019-11-16 19:10:04 -0700

    My First Commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let qaArray=[
+    {
+        question:"How many disciples did Jesus choose",
+        options=["9","12","13","15"],
+        answer:"12"
+    },
+    {
+        question:"Which King gave the order to put Daniel in the Lion's den",
+        options=["Nebuchadnezzer","Caesar Augustus","Darius","Herod"],
+        answer:"Darius"
+    },
+    {
+        question:"Who is considered as Jesus' favorite disciple",
+        options=["John","Peter","James","Matthew"],
+        answer:"John"
+    },
+    {
+        question:"Where was Jesus born",
+        options=["Egypt","Bethlehem","Nazareth","Canaan"],
+        answer:"Bethlehem"
+    },
+    {
+        question:"What was the name of the disciple who betrayed Jesus",
+        options=["John","Peter","James","Judas"],
+        answer:"Judas"
+    },
+    {
+        question:"What is the last book of the Old Testament",
+        options=["Micah","Malachi","Hosea","Habbakuk"],
+        answer:"Malachi"
+    },
+    {
+        question:"How many books are there in the Bible",
+        options=["27","32","64","66"],
+        answer:"66"
+    },
+    {
+        question:"What does God look at when people look at the outside appearance",
+        options=["skin","heart","soul","mind"],
+        answer:"heart"
+    },
+    {
+        question:"What must we do do be saved",
+        options=["Believe that Jesus is Lord","Forgive others' sins","Give alms to the poor","Read the Bible everyday"],
+        answer:"Believe that Jesus is Lord"
+    },
+    {
+        question:"Who are Noah's sons",
+        options=["Japheth, Shem & Cain","Cain, Abel & Ham","Shem, Ham & Japheth","Adam, Cain & Japheth"],
+        answer:"Shem, Ham & Japheth"
+    }
+];
+
+
+
+
