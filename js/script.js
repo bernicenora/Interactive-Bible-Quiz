6feb8302d681a2853e79a75c7f0e6fc84af49a9e
BE <bernicewebdesigner@gmail.com>
2019-11-21 09:52:31 -0700

    Displaying the score on the page after the Submit Button is pressed

--- js/script.js
+++ js/script.js
@@ -54,6 +54,8 @@ var qaArray=[
 ];
 
 //Pick a question from the array to populate the display panel
+
+//Getting all required elements from the code
 var questionNumber = document.getElementById("questionNumber");
 var questionDesc = document.getElementById("questionDesc");
 var answerchoice = document.getElementsByName("answer");
@@ -65,15 +67,20 @@ var choiceA = document.getElementById("choiceA");
 var choiceB = document.getElementById("choiceB");
 var choiceC = document.getElementById("choiceC");
 var choiceD = document.getElementById("choiceD");
-var btn = document.getElementById("next");
+var btnNext = document.getElementById("next");
+var btnSubmit = document.getElementById("submit");
+var success = document.getElementById("success");
 //var choices = document.getElementsByClassName("choices");
 
 var i=1; //counter
-var score=0; //To calculate the score
+var score=0; //Variable calculate the score
+
 // When the window loads, load the question related details
 window.onload = function(){
+    btnSubmit.disabled = true; //Disable the Submit Button
     let dis = ". "; //Append the .
     let qMark = " ?" //Append the ? symbol at the end of the question
+
     //Getting the Question 1 and the relevant Options to display on the page
     questionDesc.innerHTML = 1+dis+qaArray[0].question+qMark;
     choice1.firstChild.nodeValue = qaArray[0].options[0];
@@ -82,26 +89,103 @@ window.onload = function(){
     choice4.firstChild.nodeValue = qaArray[0].options[3];
 
     // When the NEXT button is clicked, display the next question in the array
-    btn.onclick = function(){
-        //console.log(i);
-        //Check first question's answer
-        console.log(qaArray[0].answer);
-        console.log(choiceB.checked);
-        console.log(qaArray[i].answer);
+    btnNext.onclick = function(){
 
 
+        //Checking for the right answer from the array and updating score
+
+        if (choiceA.checked){
+            if (qaArray[i-1].answer == choice1.firstChild.nodeValue){
+                score += 1;
+            }else{
+                score += 0;
+            }
+        }
+
+        if (choiceB.checked){
+            if (qaArray[i-1].answer == choice2.firstChild.nodeValue){
+                score += 1;
+            }else{
+                score += 0;
+            }
+        }
+
+        if (choiceC.checked){
+            if (qaArray[i-1].answer == choice3.firstChild.nodeValue){
+                score += 1;
+            }else{
+                score += 0;
+            }
+        }
+
+        if (choiceD.checked){
+            if (qaArray[i-1].answer == choice4.firstChild.nodeValue){
+                score += 1;
+            }else{
+                score += 0;
+            }
+        }
         //Display the question and the relevant choices
         questionDesc.innerHTML = i+1+dis+qaArray[i].question+qMark;
         choice1.firstChild.nodeValue = qaArray[i].options[0];
         choice2.firstChild.nodeValue = qaArray[i].options[1];
         choice3.firstChild.nodeValue = qaArray[i].options[2];
         choice4.firstChild.nodeValue = qaArray[i].options[3];
+
         i+=1; //increment the counter to go to the next question in the array
+        // Setting the initial state of the radio button
+        choiceA.checked = true;
         if (i>9){ // All 10 questions are done, and so check for the end of the question array
-            //Once done, log out ti console
+
+            //Once done, log out to console
             console.log("done and ready for scoring!");
             // Diable the next button and shift the focus to the SUBMIT button
-            btn.disabled = true;
-        }
+            btnNext.disabled = true; //Disable the Next Button after 9 questions
+            btnSubmit.disabled = false; //Enable the Submit Button
+            btnSubmit.onclick = function(){
+                //console.log(i);
+                if (choiceA.checked){
+                    if (qaArray[i-1].answer == choice1.firstChild.nodeValue){
+                        score += 1;
+                    }else{
+                        score += 0;
+                    }
+                }
+
+                if (choiceB.checked){
+                    if (qaArray[i-1].answer == choice2.firstChild.nodeValue){
+                        score += 1;
+                    }else{
+                        score += 0;
+                    }
+                }
+
+                if (choiceC.checked){
+                    if (qaArray[i-1].answer == choice3.firstChild.nodeValue){
+                        score += 1;
+                    }else{
+                        score += 0;
+                    }
+                }
+
+                if (choiceD.checked){
+                    if (qaArray[i-1].answer == choice4.firstChild.nodeValue){
+                        score += 1;
+                    }else{
+                        score += 0;
+                    }
+                }
+                //console.log(score);
+                //Log out the score on an alert box!
+                /*if (score>5){
+                    alert("Wohoo!! You have scored "+score+" points. Good job!!");
+                }else{
+                    alert("You have scored "+score+" points. Better luck next time!!");
+                }*/
+                console.log(score);
+                success.firstChild.nodeValue = "You have scored "+score;
+                console.log(success.firstChild.nodeValue);
+            } //End of checks for Submit Button
+        } //End of checks for last question
     }
 }
